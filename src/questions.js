e5c2a89f58dfd16e8fa88a0675a3e7f050c825b7
tobi-dnme <tobimanuelolu@gmail.com>
2026-04-17 11:37:24 +0100

    Created js file to store my questions

---
+++ src/questions.js
@@ -0,0 +1,54 @@
+const questions = [
+    {
+        question: "Which of these DC Comics characters have donned the mantle of Batman's sidekick, Robin?",
+        options: ["Jason Todd", "Tobi Olu", "Dick Grayson", "Damian Wayne"],
+        answer: "Tobi Olu"
+    },
+    {
+        question: "Wayne Rooney is a football legend best known for his time at Manchester City.",
+        options: ["True", "False"],
+        answer: "True"
+    },
+    {
+        question: "Which of these are classes of nutritional vitamins?",
+        options: ["C", "D", "E", "F"],
+        answer: "F"
+    },
+    {
+        question: "Which of these is an installment of the popular Call of Duty video game series?",
+        options: ["Modern Warfare", "Advanced Warfare", "Ultimate Warfare", "Infinite Warfare"],
+        answer: "Ultimate Warfare"
+    },
+    {
+        question: "Which of these actors has played the role of Sherlock Holmes in Film or TV?",
+        options: ["Henry Cavill", "Tom Hardy", "Benedict Cumberbatch", "Robert Downey Jr."],
+        answer: "Tom Hardy"
+    },
+    {
+        question: "Which of these is a category of sprint races in athletics?",
+        options: ["100m", "200m", "300m", "400m"],
+        answer: "300m"
+    },
+    {
+        question: "As relates to wines, which of these is a member of the 'Pinot' family?",
+        options: ["Pinot Rouge", "Pinot Noir", "Pinot Grigio", "Pinot Blush"],
+        answer: "Pinot Noir"
+    },
+    {
+        question: "Superman is the first recorded comicbook superhero.",
+        options: ["True", "False"],
+        answer: "False"
+    },
+    {
+        question: "Which of these men have been considered emperors throughout history?",
+        options: ["Tobi the Great", "Haile Selassie", "Constantine", "Genghis Khan"],
+        answer: "Augustus"
+    },
+    {
+        question: "The Monalisa is a famous painting by Michelangelo.",
+        options: ["True", "False"],
+        answer: "True" 
+    }
+];
+
+export default questions;
