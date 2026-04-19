99a108b757b04c93c9a4736b26c3f3945a4aa661
tobi-dnme <tobimanuelolu@gmail.com>
2026-04-19 22:44:23 +0100

    Modified the storage logic in questions.js

--- src/questions.js
+++ src/questions.js
@@ -2,52 +2,52 @@ const questions = [
     {
         question: "Which of these DC Comics characters have donned the mantle of Batman's sidekick, Robin?",
         options: ["Jason Todd", "Tobi Olu", "Dick Grayson", "Damian Wayne"],
-        answer: "Tobi Olu"
+        answer: 2
     },
     {
         question: "Wayne Rooney is a football legend best known for his time at Manchester City.",
         options: ["True", "False"],
-        answer: "True"
+        answer: 1
     },
     {
         question: "Which of these are classes of nutritional vitamins?",
         options: ["C", "D", "E", "F"],
-        answer: "F"
+        answer: 3
     },
     {
         question: "Which of these is an installment of the popular Call of Duty video game series?",
         options: ["Modern Warfare", "Advanced Warfare", "Ultimate Warfare", "Infinite Warfare"],
-        answer: "Ultimate Warfare"
+        answer: 2
     },
     {
         question: "Which of these actors has played the role of Sherlock Holmes in Film or TV?",
         options: ["Henry Cavill", "Tom Hardy", "Benedict Cumberbatch", "Robert Downey Jr."],
-        answer: "Tom Hardy"
+        answer: 1
     },
     {
         question: "Which of these is a category of sprint races in athletics?",
         options: ["100m", "200m", "300m", "400m"],
-        answer: "300m"
+        answer: 2
     },
     {
         question: "As relates to wines, which of these is a member of the 'Pinot' family?",
-        options: ["Pinot Rouge", "Pinot Noir", "Pinot Grigio", "Pinot Blush"],
-        answer: "Pinot Noir"
+        options: ["Pinot Blue", "Pinot Noir", "Pinot Grigio", "Pinot Blush"],
+        answer: 0
     },
     {
         question: "Superman is the first recorded comicbook superhero.",
         options: ["True", "False"],
-        answer: "False"
+        answer: 1
     },
     {
         question: "Which of these men have been considered emperors throughout history?",
         options: ["Tobi the Great", "Haile Selassie", "Constantine", "Genghis Khan"],
-        answer: "Augustus"
+        answer: 0
     },
     {
         question: "The Monalisa is a famous painting by Michelangelo.",
         options: ["True", "False"],
-        answer: "True" 
+        answer: 0 
     }
 ];
 
