4dbaa699a939e675e106261d17463d17751ebe04
tobi-dnme <tobimanuelolu@gmail.com>
2026-04-21 12:32:53 +0100

    Created beginQuiz function with shuffled questions, and injecting it to the html

--- src/app.js
+++ src/app.js
@@ -2,3 +2,13 @@ import questions from "./questions";
 
 let currentQuestionIndex = 0;
 let currentScore = 0;
+
+function beginQuiz() {
+  document.getElementById("intro").classList.add("hidden");
+  document.getElementById("quiz").classList.remove("hidden");
+
+  questions.sort(() => Math.random() - 0.5);
+
+  currentQuestionIndex = 0;
+  currentScore = 0;
+}
