25e603470811d8261ef4d456250985fbf4504b34
tobi-dnme <tobimanuelolu@gmail.com>
2026-04-30 12:24:28 +0100

    Created showQuestion function and updated beginQuiz() accordingly

--- src/app.js
+++ src/app.js
@@ -3,12 +3,33 @@ import questions from "./questions";
 let currentQuestionIndex = 0;
 let currentScore = 0;
 
-function beginQuiz() {
+export const beginQuiz = () => {
   document.getElementById("intro").classList.add("hidden");
   document.getElementById("quiz").classList.remove("hidden");
+
 
   questions.sort(() => Math.random() - 0.5);
 
   currentQuestionIndex = 0;
   currentScore = 0;
+
+  showQuestion();
+};
+
+const showQuestion = () => {
+  const myQuestion = questions[currentQuestionIndex];
+
+  const questionElement = document.getElementById("question");
+  questionElement.textContent = myQuestion.question;
+
+  const optionsContainer = document.getElementById("options");
+  optionsContainer.innerHTML = "";
+
+  for (let i = 0; i < myQuestion.options.length; i++) {
+
+    const button = document.createElement("button");
+    button.textContent = myQuestion.options[i];
+  
+    optionsContainer.appendChild(button);
+  }
 }
